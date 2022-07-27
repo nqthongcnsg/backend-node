ee9033797b41960eb626ea4eeedd9da9e4d57110
Quang Thong <nqthongcnsg@gmail.com>
2022-07-27 23:22:48 +0700

    deloy

--- models/dbConnect.js
+++ models/dbConnect.js
@@ -1,7 +1,7 @@
 const mysql= require('mysql')
 
 
-const connection= mysql.createConnection({host: "localhost",user:"root", password:"", database:"shop"})
+const connection= mysql.createConnection({host: "us-cdbr-east-06.cleardb.net",user:"bccb90af80e10c", password:"90a67e67", database:"heroku_c23a03559c811d3"})
 connection.connect();
 
 module.exports= connection
